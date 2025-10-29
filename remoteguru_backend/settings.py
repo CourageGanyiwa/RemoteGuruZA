e674a4f449d94fadbf5fe04df9446952b3438978
Christian Courage <Couresma@gmail.com>
2025-10-29 21:47:00 +0200

    Update DATABASES configuration for SSL support

--- remoteguru_backend/settings.py
+++ remoteguru_backend/settings.py
@@ -113,12 +113,13 @@ SECRET_KEY = env('SECRET_KEY')
 
 
 DATABASES = {
-    'default': dj_database_url.config(
-    default=os.environ.get('DATABASE_URL'),
+    "default": dj_database_url.parse(
+        os.environ.get("DATABASE_URL"),
         conn_max_age=600,
         ssl_require=True
     )
 }
+
 
 
 # Password validation
